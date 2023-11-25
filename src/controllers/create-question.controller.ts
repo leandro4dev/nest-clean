8372fc4c1d167f4ef9eb3f32231de3b7174f1352
leandro4silva <leandro94.a.s@gmail.com>
2023-11-25 14:24:56 -0300

    chore: config vitest

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
-import { PrismaService } from "src/prisma/prisma.service";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
+import { PrismaService } from "@/prisma/prisma.service";
 import { z } from "zod";
-import { CurrentUser } from "src/auth/current-user-decorator";
-import { UserPaylod } from "src/auth/jwt.strategy";
+import { CurrentUser } from "@/auth/current-user-decorator";
+import { UserPaylod } from "@/auth/jwt.strategy";
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
