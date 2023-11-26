3903b45e10fcbfd9c23e7a51c3a397dbb908f79c
leandro4silva <leandro94.a.s@gmail.com>
2023-11-26 12:53:48 -0300

    feat: fetch question and create question tests e2e

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -22,7 +22,7 @@ export class FethRecentQuestionsController {
 
   @Get()
   async handle(@Query("page", queryValidationPipe) page: PageQueryParamSchema) {
-    const perPage = 2;
+    const perPage = 20;
 
     const questions = await this.prisma.question.findMany({
       take: perPage,
