febd0def120916973e9441479805a1b007658f99
leandro4silva <leandro94.a.s@gmail.com>
2023-11-27 21:05:32 -0300

    feat: infra layer

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
-import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
-import { PrismaService } from "@/prisma/prisma.service";
+import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { PrismaService } from "@/infra/prisma/prisma.service";
 import { z } from "zod";
-import { CurrentUser } from "@/auth/current-user-decorator";
-import { UserPaylod } from "@/auth/jwt.strategy";
+import { CurrentUser } from "@/infra/auth/current-user-decorator";
+import { UserPaylod } from "@/infra/auth/jwt.strategy";
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
