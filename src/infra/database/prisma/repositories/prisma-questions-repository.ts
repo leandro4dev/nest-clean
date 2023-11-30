136199bb4c985486437e1317a72de348b91eb719
leandro4silva <leandro94.a.s@gmail.com>
2023-11-30 18:13:24 -0300

    feat: add mapper and implement prisma repository

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,11 +2,25 @@ import { PaginatinParams } from "@/core/repositories/pagination-params";
 import { QuestionsRespository } from "@/domain/forum/application/repositories/question-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRespository {
-  findById(id: string): Promise<Question | null> {
-    throw new Error("Method not implemented.");
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionMapper.toDomain(question);
   }
 
   findManyRecent(params: PaginatinParams): Promise<Question[]> {
