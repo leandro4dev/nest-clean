241593edc8d6d076bbd3b45e3097a5acad78547e
Leandro A. Silva <leandro94.a.s@gmail.com>
2023-12-11 21:32:57 -0300

    feat: implements controller get question by slug

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -2,6 +2,7 @@ import { Either, left, right } from "@/core/either";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { QuestionsRespository } from "@/domain/forum/application/repositories/question-repository";
+import { Injectable } from "@nestjs/common";
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string;
@@ -14,12 +15,15 @@ type GetQuestionBySlugUseCaseResponse = Either<
   }
 >;
 
+@Injectable()
 export class GetQuestionBySlugUseCase {
   constructor(private questionsRepository: QuestionsRespository) {}
 
   async execute({
     slug,
   }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    console.log(slug);
+
     const question = await this.questionsRepository.findBySlug(slug);
 
     if (!question) {
---
+++ src/infra/http/controllers/get-question-by-slug.controller.ts
@@ -0,0 +1,37 @@
+import { ResourceNotFoundError } from "@/domain/forum/application/use-cases/errors/resource-not-found-error";
+import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug";
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+} from "@nestjs/common";
+import { QuestionPresenter } from "../presenters/question-presenter";
+
+@Controller("/questions/:slug")
+export class GetQuestionBySlugController {
+  constructor(private getQuestionBySlug: GetQuestionBySlugUseCase) {}
+
+  @Get()
+  async handle(@Param("slug") slug: string) {
+    const result = await this.getQuestionBySlug.execute({
+      slug,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+
+    const { question } = result.value;
+
+    return { question: QuestionPresenter.toHTTP(question) };
+  }
+}
