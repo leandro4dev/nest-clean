70a0129f6bdbddee4b224327d3084ac2004c33de
Leandro A. Silva <leandro94.a.s@gmail.com>
2023-12-13 19:09:38 -0300

    fix: implement injectable to use case edit question

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -11,12 +11,15 @@ import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/reg
 import { CryptographyModule } from "../cryptography/cryptography.module";
 import { GetQuestionBySlugController } from "./controllers/get-question-by-slug.controller";
 import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug";
+import { EditQuestionController } from "./controllers/edit-question.controller";
+import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-question";
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [
     AuthenticateController,
     CreateAccountController,
+    EditQuestionController,
     CreateQuestionController,
     FethRecentQuestionsController,
     GetQuestionBySlugController,
@@ -24,6 +27,7 @@ import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/g
   providers: [
     CreateQuestionUseCase,
     RegisterStudentUseCase,
+    EditQuestionUseCase,
     FetchRecentQuestionsUseCase,
     AuthenticateStudentUseCase,
     GetQuestionBySlugUseCase,
