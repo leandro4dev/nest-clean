9b31cb8b67b1f19c46f25567a706613a81dff28d
Leandro A. Silva <leandro94.a.s@gmail.com>
2023-12-17 00:37:40 -0300

    feat: implements edit answer controller

--- src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
@@ -1,23 +1,27 @@
+import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 import { AppModule } from "@/infra/app.module";
 import { DatabaseModule } from "@/infra/database/database.module";
 import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
+import request from "supertest";
 import { AnswerFactory } from "test/factories/make-answer";
+import { QuestionFactory } from "test/factories/make-question";
 import { StudentFactory } from "test/factories/make-student";
 
 describe("Edit Answer (E2E)", () => {
   let app: INestApplication;
   let prisma: PrismaService;
   let jwt: JwtService;
+  let questionFactory: QuestionFactory;
   let studentFactory: StudentFactory;
   let answerFactory: AnswerFactory;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, AnswerFactory],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
@@ -25,10 +29,44 @@ describe("Edit Answer (E2E)", () => {
     jwt = moduleRef.get(JwtService);
 
     studentFactory = moduleRef.get(StudentFactory);
+    questionFactory = moduleRef.get(QuestionFactory);
     answerFactory = moduleRef.get(AnswerFactory);
 
     await app.init();
   });
 
-  test("[PUT] /answers/:id", async () => {});
+  test("[PUT] /answers/:id", async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const question = await questionFactory.makePrismaQuestion({
+      title: "Question 01",
+      slug: Slug.create("question-01"),
+      authorId: user.id,
+    });
+
+    const answer = await answerFactory.makePrismaAnswer({
+      content: "Example Answer",
+      authorId: user.id,
+      questionId: question.id,
+    });
+
+    const response = await request(app.getHttpServer())
+      .put(`/answers/${answer.id}`)
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        content: "New Example Content",
+      });
+
+    expect(response.statusCode).toBe(204);
+
+    const asnwerOnDatabase = await prisma.answer.findFirst({
+      where: {
+        content: "New Example Content",
+      },
+    });
+
+    expect(asnwerOnDatabase).toBeTruthy();
+  });
 });
