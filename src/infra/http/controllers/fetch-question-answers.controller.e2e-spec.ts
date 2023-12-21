90004ddde2c0a835b125fbcc42a2b304eb8a66e6
Leandro A. Silva <leandro94.a.s@gmail.com>
2023-12-21 19:24:35 -0300

    feat: implements fetch question answer and tests

---
+++ src/infra/http/controllers/fetch-question-answers.controller.e2e-spec.ts
@@ -0,0 +1,75 @@
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import { AnswerFactory } from "test/factories/make-answer";
+import { QuestionFactory } from "test/factories/make-question";
+import { StudentFactory } from "test/factories/make-student";
+import request from "supertest";
+
+describe("Fetch Question Answers (E2E)", () => {
+  let app: INestApplication;
+  let jwt: JwtService;
+  let studentFactory: StudentFactory;
+  let answerFactory: AnswerFactory;
+  let questionFactory: QuestionFactory;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    jwt = moduleRef.get(JwtService);
+
+    studentFactory = moduleRef.get(StudentFactory);
+    questionFactory = moduleRef.get(QuestionFactory);
+    answerFactory = moduleRef.get(AnswerFactory);
+
+    await app.init();
+  });
+
+  test("[GET] /questions/:questionId/answers", async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    });
+
+    await Promise.all([
+      answerFactory.makePrismaAnswer({
+        content: "Answer 1",
+        authorId: user.id,
+        questionId: question.id,
+      }),
+      answerFactory.makePrismaAnswer({
+        content: "Answer 2",
+        authorId: user.id,
+        questionId: question.id,
+      }),
+    ]);
+
+    const questionId = question.id.toString();
+
+    const response = await request(app.getHttpServer())
+      .get(`/questions/${questionId}/answers`)
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send();
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      answers: expect.arrayContaining([
+        expect.objectContaining({
+          content: "Answer 1",
+        }),
+        expect.objectContaining({
+          content: "Answer 2",
+        }),
+      ]),
+    });
+  });
+});
