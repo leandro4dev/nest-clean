f672d6c896be795130066ab7992234cd61877f00
Leandro A. Silva <leandro94.a.s@gmail.com>
2023-12-26 00:10:46 -0300

    fix: environment variables

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -8,7 +8,6 @@ export const envSchema = z.object({
   AWS_BUCKET_NAME: z.string(),
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
-  AWS_ACCESS_POINT: z.string(),
   AWS_ACCESS_REGION: z.string(),
 });
 
