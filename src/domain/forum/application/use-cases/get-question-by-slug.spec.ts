2d4409820f89009967c84e36c7f653d25fb2954e
Leandro A. Silva <leandro94.a.s@gmail.com>
2023-12-31 14:56:21 -0300

    fix: get question by slug with details

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -3,9 +3,16 @@ import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
 import { Slug } from "../../enterprise/entities/value-objects/slug";
 import { makeQuestion } from "test/factories/make-question";
 import { beforeEach } from "vitest";
+import { InMemoryAttachmentRepository } from "test/repositories/in-memory-attachment-repository";
 import { InMemoryQuestionsAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository";
+import { makeStudent } from "test/factories/make-student";
+import { makeAttachment } from "test/factories/make-attachment";
+import { makeQuestionAttachments } from "test/factories/make-question-attachments";
 
 let inMemoryQuestionAttachmentsRepository: InMemoryQuestionsAttachmentsRepository;
+let inMemoryAttachmentRepository: InMemoryAttachmentRepository;
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: GetQuestionBySlugUseCase;
 
@@ -14,18 +21,43 @@ describe("Get Question By Slug", () => {
     inMemoryQuestionAttachmentsRepository =
       new InMemoryQuestionsAttachmentsRepository();
 
+    inMemoryAttachmentRepository = new InMemoryAttachmentRepository();
+
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
+
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentsRepository,
+      inMemoryAttachmentRepository,
+      inMemoryStudentsRepository,
     );
+
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository); // system under test
   });
 
   it("should be able to get a question by slug", async () => {
+    const student = makeStudent({ name: "John Doe" });
+
+    inMemoryStudentsRepository.items.push(student);
+
     const newQuestion = makeQuestion({
       slug: Slug.create("example-question"),
+      authorId: student.id,
     });
 
     await inMemoryQuestionsRepository.create(newQuestion);
+
+    const attachment = makeAttachment({
+      title: "Some attachment",
+    });
+
+    inMemoryAttachmentRepository.items.push(attachment);
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachments({
+        attachmentId: attachment.id,
+        questionId: newQuestion.id,
+      }),
+    );
 
     const result = await sut.execute({
       slug: "example-question",
@@ -34,9 +66,17 @@ describe("Get Question By Slug", () => {
     expect(result.isRight()).toBe(true);
 
     if (result.isRight()) {
-      expect(inMemoryQuestionsRepository.items[0]).toEqual(
-        result.value.question,
-      );
+      expect(result.value).toMatchObject({
+        question: expect.objectContaining({
+          title: newQuestion.title,
+          author: "John Doe",
+          attachments: [
+            expect.objectContaining({
+              title: "Some attachment",
+            }),
+          ],
+        }),
+      });
     }
   });
 });
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,15 +1,71 @@
 import { DomainEvents } from "@/core/events/domain-events";
 import { PaginatinParams } from "@/core/repositories/pagination-params";
-import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionsRespository } from "@/domain/forum/application/repositories/question-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
+import { QuestionDetails } from "@/domain/forum/enterprise/entities/value-objects/question-details";
+import { InMemoryStudentsRepository } from "./in-memory-students-repository";
+import { InMemoryAttachmentRepository } from "./in-memory-attachment-repository";
+import { InMemoryQuestionsAttachmentsRepository } from "./in-memory-question-attachments-repository";
 
 export class InMemoryQuestionsRepository implements QuestionsRespository {
   public items: Question[] = [];
 
   constructor(
-    private questionAttachmentRepository: QuestionAttachmentsRepository,
+    private questionAttachmentRepository: InMemoryQuestionsAttachmentsRepository,
+    private attachmentRepository: InMemoryAttachmentRepository,
+    private studentRepository: InMemoryStudentsRepository,
   ) {}
+
+  async findDetailsBySlug(slug: string): Promise<QuestionDetails | null> {
+    const question = this.items.find((item) => item.slug.value === slug);
+
+    if (!question) {
+      return null;
+    }
+
+    const author = this.studentRepository.items.find((student) => {
+      return student.id.equals(question.authorId);
+    });
+
+    if (!author) {
+      throw new Error(
+        `Author with ID ${question.authorId.toString()} does not exist`,
+      );
+    }
+
+    const questionAttachments = this.questionAttachmentRepository.items.filter(
+      (questionAttachment) => {
+        return questionAttachment.questionId.equals(question.id);
+      },
+    );
+
+    const attachments = questionAttachments.map((questionAttachment) => {
+      const attachment = this.attachmentRepository.items.find((attachment) => {
+        return attachment.id.equals(questionAttachment.attachmentId);
+      });
+
+      if (!attachment) {
+        throw new Error(
+          `Attachment with ID ${questionAttachment.attachmentId.toString()} does not exist`,
+        );
+      }
+
+      return attachment;
+    });
+
+    return QuestionDetails.create({
+      questionId: question.id,
+      authorId: question.authorId,
+      author: author.name,
+      title: question.title,
+      slug: question.slug,
+      content: question.content,
+      bestAnswerId: question.bestAnswerId,
+      attachments,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    });
+  }
 
   async findManyRecent({ page }: PaginatinParams): Promise<Question[]> {
     const questions = this.items
