8293135c7e273f7da2d1364f87da5c1e051e15a9
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-01 23:25:13 -0300

    feat: implements method to find details by slug

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -5,6 +5,8 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
 import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
+import { QuestionDetails } from "@/domain/forum/enterprise/entities/value-objects/question-details";
+import { PrismaQuestionDetailsMapper } from "../mappers/prisma-question-details-mapper";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRespository {
@@ -12,6 +14,24 @@ export class PrismaQuestionsRepository implements QuestionsRespository {
     private prisma: PrismaService,
     private questionAttachmentRepository: QuestionAttachmentsRepository,
   ) {}
+
+  async findDetailsBySlug(slug: string): Promise<QuestionDetails | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      },
+      include: {
+        author: true,
+        attachments: true,
+      },
+    });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionDetailsMapper.toDomain(question);
+  }
 
   async findById(id: string): Promise<Question | null> {
     const question = await this.prisma.question.findUnique({
