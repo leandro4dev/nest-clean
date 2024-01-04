94f118e38296e8ea80791446630821aa8784e730
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-04 19:38:48 -0300

    feat: implements make prisma notification factory

--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -4,6 +4,9 @@ import {
   Notification,
   NotificationProps,
 } from "@/domain/notification/enterprise/entities/notification";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
+import { PrismaNotificationMapper } from "@/infra/database/prisma/mappers/prisma-notification-mapper";
 
 export function makeNotification(
   override: Partial<NotificationProps> = {},
@@ -20,4 +23,21 @@ export function makeNotification(
   );
 
   return question;
+}
+
+@Injectable()
+export class NotificationFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaNotification(
+    data: Partial<NotificationProps> = {},
+  ): Promise<Notification> {
+    const notification = makeNotification(data);
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPrisma(notification),
+    });
+
+    return notification;
+  }
 }
